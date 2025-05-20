e4df064ab20f8a7cb7739181d97308e94d5e1bcc
ggogogohub <badshasonu020@gmail.com>
2025-05-20 23:57:18 +0100

    Updates

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,16 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import GlobalStyles from './styles/GlobalStyles';
+import './index.css';
 import { ThemeProvider } from 'styled-components';
-import theme from './styles/theme';
+import { theme } from './styles/theme';
+import GlobalStyles from './styles/GlobalStyles';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
       <App />
     </ThemeProvider>
-  </StrictMode>,
-)
+  </React.StrictMode>,
+);
